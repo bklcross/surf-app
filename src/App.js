9997116a8187bc4c6b8650825699c694d19fa877
Chris Mendoza <cmend85@gmail.com>
2018-03-24 00:50:25 -0500

    Added semantic ui package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React, { Component } from 'react';
+
+import { Button } from 'semantic-ui-react'
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Button primary content='Hey we made it' />
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
